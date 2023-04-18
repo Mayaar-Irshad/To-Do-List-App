8acbfec0d19f225860913da999af74956abc7dc8
dlnishi <daniel.nishi9@gmail.com>
2023-04-18 12:28:31 -0700

    added addtaskwindow class

---
+++ todoapp/AddTaskWindow.java
@@ -0,0 +1,22 @@
+package todoapp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class AddTaskWindow extends JFrame {
+    
+    public AddTaskWindow() {
+        super();
+        setupLayout();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void setupLayout() {
+        Container contentPane = getContenPane();
+        
+    }
+
+    
+}
